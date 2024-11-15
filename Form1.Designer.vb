2b6be318b22181177412d03c0ceb661bf464ade5
Angel Mae Carandang <angelmaecarandang@gmail.com>
2024-11-15 12:36:48 +0800

    Added login

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,32 +23,92 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         btnTest = New Button()
+        Username = New Label()
+        TextBox_Un = New TextBox()
+        Password = New Label()
+        TextBox_Pass = New TextBox()
+        Login = New Button()
         SuspendLayout()
         ' 
         ' btnTest
         ' 
         btnTest.BackColor = SystemColors.ControlLight
-        btnTest.Location = New Point(83, 133)
+        btnTest.Location = New Point(9, 9)
         btnTest.Margin = New Padding(0)
         btnTest.Name = "btnTest"
-        btnTest.Size = New Size(245, 83)
+        btnTest.Size = New Size(600, 40)
         btnTest.TabIndex = 0
-        btnTest.Text = "MYSQL TEST"
+        btnTest.Text = "CHECK CONNECTION"
         btnTest.UseVisualStyleBackColor = False
+        ' 
+        ' Username
+        ' 
+        Username.Font = New Font("Times New Roman", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Username.Location = New Point(115, 90)
+        Username.Name = "Username"
+        Username.Size = New Size(165, 28)
+        Username.TabIndex = 1
+        Username.Text = "Username"
+        ' 
+        ' TextBox_Un
+        ' 
+        TextBox_Un.Location = New Point(246, 87)
+        TextBox_Un.Multiline = True
+        TextBox_Un.Name = "TextBox_Un"
+        TextBox_Un.Size = New Size(244, 31)
+        TextBox_Un.TabIndex = 2
+        ' 
+        ' Password
+        ' 
+        Password.Font = New Font("Times New Roman", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Password.Location = New Point(115, 127)
+        Password.Name = "Password"
+        Password.Size = New Size(145, 33)
+        Password.TabIndex = 3
+        Password.Text = "Password"
+        ' 
+        ' TextBox_Pass
+        ' 
+        TextBox_Pass.Location = New Point(246, 124)
+        TextBox_Pass.Multiline = True
+        TextBox_Pass.Name = "TextBox_Pass"
+        TextBox_Pass.PasswordChar = "*"c
+        TextBox_Pass.Size = New Size(244, 31)
+        TextBox_Pass.TabIndex = 4
+        ' 
+        ' Login
+        ' 
+        Login.Location = New Point(284, 173)
+        Login.Name = "Login"
+        Login.Size = New Size(112, 37)
+        Login.TabIndex = 5
+        Login.Text = "Login"
+        Login.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(14F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(410, 373)
+        ClientSize = New Size(618, 284)
+        Controls.Add(Login)
+        Controls.Add(TextBox_Pass)
+        Controls.Add(Password)
+        Controls.Add(TextBox_Un)
+        Controls.Add(Username)
         Controls.Add(btnTest)
         Font = New Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         Margin = New Padding(5, 4, 5, 4)
         Name = "Form1"
         Text = "MYSQL DATABASE CONNECTION"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents btnTest As Button
+    Friend WithEvents Username As Label
+    Friend WithEvents TextBox_Un As TextBox
+    Friend WithEvents Password As Label
+    Friend WithEvents TextBox_Pass As TextBox
+    Friend WithEvents Login As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -4,11 +4,40 @@ Public Class Form1
     Dim COMMAND As MySqlCommand
     Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
         conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost;userid=root;password='';database=test"
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=white"
 
         Try
             conn.Open()
             MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost;userid=root;password='';database=white"
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "select * from white.edata where user_name='" & TextBox_Un.Text & "' and password='" & TextBox_Pass.Text & "' "
+            COMMAND = New MySqlCommand(Query, conn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+            While READER.Read
+                count = count + 1
+            End While
+            If count = 1 Then
+                MessageBox.Show("Username and Password are correct!")
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and Password are duplicate.")
+            Else
+                MessageBox.Show("Username and Password are not correct.")
+            End If
             conn.Close()
         Catch ex As Exception
             MsgBox(ex.Message)
